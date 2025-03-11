620f7a50c9c9114705d63802ba64b83e7901e9e0
Ngoc <zaihole2003@gmail.com>
2025-03-11 09:07:18 +0700

    update CustomerType them guest

--- src/main/java/ezcloud/message/booking/CustomerType.java
+++ src/main/java/ezcloud/message/booking/CustomerType.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 @Getter
 public enum CustomerType {
     CUSTOMER(0),
-    STAFF(1);
-
+    STAFF(1),
+    GUEST(2);
     private final int value;
 
     CustomerType(int value) {
